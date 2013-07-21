1992c9e420c8470e38466dcdc7102cc1235a5afd
Chris Brody <chris.brody@gmail.com>
2013-07-21 07:42:20 +0200

    Simple db test with bind functions

--- src/org/sqlg/SQLiteGlueTest.java
+++ src/org/sqlg/SQLiteGlueTest.java
@@ -55,6 +55,73 @@ public class SQLiteGlueTest extends Activity
     android.util.Log.i("SQLiteGlueTest", "upper: " + first);
 
     SQLiteGlue.sqlg_st_finish(st);
+
+    st = SQLiteGlue.sqlg_db_prepare_st(mydb, "drop table if exists test_table;");
+    if (st < 0) {
+      android.util.Log.w("SQLiteGlueTest", "prepare statement error: " + -st);
+      SQLiteGlue.sqlg_db_close(mydb);
+      return;
+    }
+    SQLiteGlue.sqlg_st_step(st);
+    SQLiteGlue.sqlg_st_finish(st);
+
+    // source: https://github.com/pgsqlite/PG-SQLitePlugin-iOS
+    st = SQLiteGlue.sqlg_db_prepare_st(mydb, "CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)");
+    if (st < 0) {
+      android.util.Log.w("SQLiteGlueTest", "prepare statement error: " + -st);
+      SQLiteGlue.sqlg_db_close(mydb);
+      return;
+    }
+    SQLiteGlue.sqlg_st_step(st);
+    SQLiteGlue.sqlg_st_finish(st);
+
+    st = SQLiteGlue.sqlg_db_prepare_st(mydb, "INSERT INTO test_table (data, data_num) VALUES (?,?)");
+    if (st < 0) {
+      android.util.Log.w("SQLiteGlueTest", "prepare statement error: " + -st);
+      SQLiteGlue.sqlg_db_close(mydb);
+      return;
+    }
+    SQLiteGlue.sqlg_st_bind_text(st, 1, "test");
+    SQLiteGlue.sqlg_st_bind_int(st, 2, 100);
+    int sr = SQLiteGlue.sqlg_st_step(st);
+    while (sr == 100) {
+      android.util.Log.i("SQLiteGlueTest", "step next");
+      sr = SQLiteGlue.sqlg_st_step(st);
+    }
+    android.util.Log.i("SQLiteGlueTest", "last step " + sr);
+    SQLiteGlue.sqlg_st_finish(st);
+
+    st = SQLiteGlue.sqlg_db_prepare_st(mydb, "select * from test_table;");
+    if (st < 0) {
+      android.util.Log.w("SQLiteGlueTest", "prepare statement error: " + -st);
+      SQLiteGlue.sqlg_db_close(mydb);
+      return;
+    }
+
+    sr = SQLiteGlue.sqlg_st_step(st);
+    while (sr == 100) {
+      android.util.Log.i("SQLiteGlueTest", "step next");
+
+      colcount = SQLiteGlue.sqlg_st_column_count(st);
+      android.util.Log.i("SQLiteGlueTest", "column count: " + colcount);
+
+      for (int i=0;i<colcount;++i) {
+        colname = SQLiteGlue.sqlg_st_column_name(st, i);
+        android.util.Log.i("SQLiteGlueTest", "column " + i + " name: " + colname);
+
+        coltype = SQLiteGlue.sqlg_st_column_type(st, i);
+        android.util.Log.i("SQLiteGlueTest", "column " + i + " type: " + coltype);
+
+        String text = SQLiteGlue.sqlg_st_column_text(st, i);
+        android.util.Log.i("SQLiteGlueTest", "col " + i + " text " + text);
+      }
+
+      sr = SQLiteGlue.sqlg_st_step(st);
+    }
+    android.util.Log.i("SQLiteGlueTest", "last step " + sr);
+
+    SQLiteGlue.sqlg_st_finish(st);
+
     SQLiteGlue.sqlg_db_close(mydb);
   }
 }
