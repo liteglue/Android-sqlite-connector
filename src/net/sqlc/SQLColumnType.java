37a9941bf58303666da5506a8a407c24bdfa6fe0
Chris Brody <brodybits@litehelpers.net>
2015-06-29 19:14:56 +0200

    Add SQLColumnType

---
+++ src/net/sqlc/SQLColumnType.java
@@ -0,0 +1,9 @@
+package net.sqlc;
+
+public class SQLColumnType {
+  public static final int INTEGER = 1; // SQLITE_INTEGER
+  public static final int REAL    = 2; // SQLITE_FLOAT
+  public static final int TEXT    = 3;
+  public static final int BLOB    = 4;
+  public static final int NULL    = 5;
+}
