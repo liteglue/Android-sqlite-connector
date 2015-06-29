d901bbe548919d6cd2409b25225b522751d94303
Chris Brody <brodybits@litehelpers.net>
2015-06-29 17:10:59 +0200

    Rename test project (with help from android command line)

--- src/com/test/SQLiteConnectorTest.java
+++ src/com/test/SQLiteConnectorTest.java
@@ -1,4 +1,4 @@
-package org.sqlg;
+package com.test;
 
 import android.app.Activity;
 import android.os.Bundle;
@@ -14,7 +14,7 @@ import java.io.File;
 
 import java.sql.SQLException;
 
-public class SQLiteGlueTest extends Activity
+public class SQLiteConnectorTest extends Activity
 {
   ArrayAdapter<String> resultsAdapter;
 
